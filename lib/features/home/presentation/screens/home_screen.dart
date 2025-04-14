c8f88747423c43307561b8910c43a4db7fe3a435
AmrAshraf-dev <amrashraf777aa@gmail.com>
2025-04-14 13:08:13 +0200

    //

--- lib/features/home/presentation/screens/home_screen.dart
+++ lib/features/home/presentation/screens/home_screen.dart
@@ -37,6 +37,7 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   void initState() {
     print('TOKEN : ${LocalData.getUser()?.tokenInfo.token ?? ''}');
+    //
     super.initState();
   }
 
